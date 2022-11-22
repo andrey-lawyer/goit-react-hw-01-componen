65eec46cebb44f4d3cd706e6eb0e61f28ee6e235
Andrey Uvarov <andrey-1@ukr.net>
2022-11-22 17:30:59 +0200

    exercis1_vers2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
+
+import user from '../user.json';
+import data from '../data.json';
+import friends from '../friends.json';
+import transactions from '../transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile cardUser={user} /> 
+      {/* <Statistics stats={data} />   */}
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
+
+/* <PageTitle text="24th Core Worlds Coalition Conference" /> */
+// <EventBoard events={upcomingEvents} />
